e4e8ad067195a884e4e35e191183d2525ccba4e3
Luis Carlos Garcia <102985938+Luis0830@users.noreply.github.com>
2022-12-19 16:42:02 -0400

    Add files via upload
    added files

---
+++ Pratice/ConsoleAppNew/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+Console.WriteLine("Escribe tu nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("Hola "  + nombre);
+
+Console.WriteLine("Que hora es?");
+var hora = Console.ReadLine();
+Console.WriteLine("La hora es " + hora);
